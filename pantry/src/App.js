b4c08f168e6abcfae2b6b0fe4752188586619ae0
Chris Gustanto <cgustanto@gmail.com>
2022-09-25 21:19:25 -0500

    Installed MUI and updated home page with basic text box

--- pantry/src/App.js
+++ pantry/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+
+      component="form"
+      sx={{
+        flexDirection: "column"
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <h2> Welcome to Pantry! </h2>
+      <TextField id="Enter ingredients" label="Enter Ingredients" variant="outlined" />
+
+    </Box>
   );
 }
 
