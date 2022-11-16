667645bec41839152f923b3c28ba862b1e8cef2b
michaelschwaab <michaelschwaab@ku.edu>
2022-11-15 20:46:44 -0600

    initial setup for mobile

---
+++ pantry/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'pantry-mobile',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
