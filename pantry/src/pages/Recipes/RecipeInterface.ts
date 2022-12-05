f18a10607268f220ea6334238e8651128794f293
JISAN6Z <max.m.dick@jbhunt.com>
2022-12-04 22:12:41 -0600

    can now display recipes from spoonacular api

---
+++ pantry/src/pages/Recipes/RecipeInterface.ts
@@ -0,0 +1,31 @@
+export interface apiRecipeInterface {
+    id:                    number;
+    title:                 string;
+    image:                 string;
+    imageType:             ImageType;
+    usedIngredientCount:   number;
+    missedIngredientCount: number;
+    missedIngredients:     SedIngredient[];
+    usedIngredients:       SedIngredient[];
+    unusedIngredients:     SedIngredient[];
+    likes:                 number;
+}
+
+export enum ImageType {
+    Jpg = "jpg",
+}
+
+export interface SedIngredient {
+    id:            number;
+    amount:        number;
+    unit:          string;
+    unitLong:      string;
+    unitShort:     string;
+    aisle:         string;
+    name:          string;
+    original:      string;
+    originalName:  string;
+    meta:          string[];
+    image:         string;
+    extendedName?: string;
+}
